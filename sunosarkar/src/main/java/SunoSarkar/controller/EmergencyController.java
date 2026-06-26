411955f9f75770c1b6cf95bf8f77211c71c555e3
Chander Parkash <khatriiiranjhi12@gmail.com>
2026-06-27 00:18:45 +0500

    Adding emergency contact controller

---
+++ sunosarkar/src/main/java/SunoSarkar/controller/EmergencyController.java
@@ -0,0 +1,26 @@
+package SunoSarkar.controller;
+
+import SunoSarkar.entity.EmergencyContact;
+import SunoSarkar.service.EmergencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/emergency")
+@CrossOrigin(origins = "*")
+public class EmergencyController {
+    @Autowired
+    private EmergencyService service;
+    @GetMapping("/{city}")
+    public ResponseEntity<List<EmergencyContact>> getByCity(@PathVariable String city){
+        return ResponseEntity.ok(service.getByCity(city));
+    }
+    @PostMapping
+    public ResponseEntity<EmergencyContact> add(EmergencyContact contact){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(contact));
+    }
+}
